5dfd35a4eab8f083ffe0f449c4c3f6b6dc2371a0
YongJin Jung <jyj1641@gmail.com>
2023-01-05 16:38:31 +0900

    Builder Pattern 1 객체 생성시 지정해야 할 인자가 많을 때사용하는 패턴
    Builder Pattern 2
    객채 생성 시 여러 단게를 순처적으로 거칠때, 이단계의 순서를
    결정해 두고 각 단계를 다양하게 구현할 수 있도록 하는 패턴

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-import builder.Car;
-import builder.CarBuilder;
+import builder.*;
 
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Main.builder1();
+        Main.builder2();
+    }
+
+    public static void builder1(){
         Car car1 = new Car("V6", true, "white", true, false);
         Car car2 = new CarBuilder()
                 .engine("V4")
@@ -25,5 +29,27 @@ public class Main {
 
         Car car3 = builder.airBag( random.nextInt(2) == 0).build();
         System.out.println(car3.toString());
+    }
+
+    public static void builder2(){
+        Data data = new Data("정용진", 40);
+
+        Builder textBuilder = new PlainTextBuilder(data);
+        Builder jsonBuilder = new JSONBuilder(data);
+        Builder xmlBuilder = new XMLBuilder(data);
+
+        Director plainDirector = new Director(textBuilder);
+        String build = plainDirector.build();
+        System.out.println("build = " + build);
+
+        Director jsonDirector = new Director(jsonBuilder);
+        String build2 = jsonDirector.build();
+        System.out.println("build2 = " + build2);
+
+        Director xmlDirector = new Director(xmlBuilder);
+        String build3 = xmlDirector.build();
+        System.out.println("build3 = " + build3);
+
+
     }
 }
--- src/builder/Director.java
+++ src/builder/Director.java
@@ -3,4 +3,16 @@ package builder;
 public class Director {
 
     private Builder builder;
+
+    public Director(Builder builder){
+        this.builder = builder;
+    }
+
+    public String build(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(builder.head());
+        sb.append(builder.body());
+        sb.append(builder.foot());
+        return sb.toString();
+    }
 }
